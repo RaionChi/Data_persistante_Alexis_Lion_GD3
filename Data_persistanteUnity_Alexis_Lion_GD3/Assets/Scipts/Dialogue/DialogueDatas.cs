8d286915e1626402085d6637b2b60f246892ffdb
ALion <alexis.lion@brassart.fr>
2023-11-29 02:12:58 +0100

    test annuler

--- Data_persistanteUnity_Alexis_Lion_GD3/Assets/Scipts/Dialogue/DialogueDatas.cs
+++
@@ -1,9 +0,0 @@
-
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "New DialogDatas")]
-public class DialogueDatas : ScriptableObject
-{
-    public SentenceDatas[] Sentences;
-
-}
--- Data_persistanteUnity_Alexis_Lion_GD3/Assets/Scipts/ITalk.cs
+++
@@ -1,5 +0,0 @@
-
-public interface ITalk 
-{
-    public void Talk();
-}
